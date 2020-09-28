ecb7563c8276266353a4befcf1990e8d2405db58
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-09-28 19:58:19 -0300

    refactor: add relation between provider and appointments

--- src/database/migrations/1601325479829-CreateAppointments.ts
+++ src/database/migrations/1601325479829-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1601325479829
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
