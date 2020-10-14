947ffee1ab49877fc8c46efbdd24a97c0e03e051
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-14 17:10:18 -0300

    test: ensure SendForgotPasswordEmailService sends an email

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,11 @@
-import FakeAppointmentsRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 import AppError from '@shared/errors/AppError';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeAppointmentsRepository();
+    const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
     const createAppointment = new CreateUserService(
       fakeUsersRepository,
@@ -22,7 +22,7 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a new user with same from another', async () => {
-    const fakeUsersRepository = new FakeAppointmentsRepository();
+    const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
     const createAppointment = new CreateUserService(
       fakeUsersRepository,
