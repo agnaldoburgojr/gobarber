80cfd97be52489e6744271a364b368e3ef31524b
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-14 13:52:10 -0300

    test: ensure AuhenticateUser does valid and invalid authentication

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import FakeAppointmentsRepository from '../repositories/fakes/FakeUsersRepositor
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateUser', () => {
   it('should be able to authenticate', async () => {
@@ -30,5 +31,49 @@ describe('CreateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with no existing user', async () => {
+    const fakeUsersRepository = new FakeAppointmentsRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const auhenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      auhenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeAppointmentsRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const auhenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      auhenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '51234567',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
