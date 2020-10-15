27d1308206ccb96a3e1996b3899d3491f0a19b5b
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-15 11:34:54 -0300

    feat: add reset password

--- src/modules/users/providers/index.ts
+++ src/modules/users/providers/index.ts
@@ -3,7 +3,4 @@ import { container } from 'tsyringe';
 import IHashProviders from './HashProvider/models/IHashProvider';
 import BCriptHashProvider from './HashProvider/implementations/BCriptyHashProvider';
 
-container.registerSingleton<IHashProviders>(
-  'BCriptHashProvider',
-  BCriptHashProvider,
-);
+container.registerSingleton<IHashProviders>('HashProvider', BCriptHashProvider);
