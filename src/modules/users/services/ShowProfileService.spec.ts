4c62da1f5cdc8d0e499a50ab91912a69f916b5ee
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2020-10-16 10:00:17 -0300

    test: add ShowProfileServie

---
+++ src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ShowProfileService from './ShowProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+
+describe('UpdateProvider', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able show profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const showedUser = await showProfile.execute({
+      user_id: user.id,
+    });
+
+    expect(showedUser.name).toBe('John Doe');
+    expect(showedUser.email).toBe('johndoe@example.com');
+  });
+
+  it('should not be able show profile if user does not exists', async () => {
+    await expect(
+      showProfile.execute({
+        user_id: 'non-existing-user',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
